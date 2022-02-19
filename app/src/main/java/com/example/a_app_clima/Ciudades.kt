3d42f07bc773fc04a69df67cb0714a9aaefdf5b2
JoseCima <josegcima@gmail.com>
2022-02-19 17:43:18 -0500

    Añadiendo Intent hemos escrito el codigo necesario para que al presionar los botones de las ciudades, nos envíe al main activity que en nuestro caso es la segunda actividad a mostrar

--- app/src/main/java/com/example/a_app_clima/Ciudades.kt
+++ app/src/main/java/com/example/a_app_clima/Ciudades.kt
@@ -1,11 +1,41 @@
 package com.example.a_app_clima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Button
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        val bPlaya = findViewById<Button>(R.id.btn_playa)
+        val bChetumal = findViewById<Button>(R.id.btn_chetumal)
+        val bCarrillo = findViewById<Button>(R.id.btn_carrillo)
+        val bCancun = findViewById<Button>(R.id.btn_cancun)
+
+        bPlaya.setOnClickListener(View.OnClickListener {
+            //hacia donde queremos ir
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        bChetumal.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        bCarrillo.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        bCancun.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
