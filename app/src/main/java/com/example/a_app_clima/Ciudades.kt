7af7e854e719bc06a5ebb24ad6234e6357862b4f
JoseCima <josegcima@gmail.com>
2022-03-01 18:59:32 -0500

    App del Clima Mejorada. Agregando botón para mostrar información sobre la ciudad de Shanghai

--- app/src/main/java/com/example/a_app_clima/Ciudades.kt
+++ app/src/main/java/com/example/a_app_clima/Ciudades.kt
@@ -21,6 +21,9 @@ class Ciudades : AppCompatActivity() {
         val bCarrillo = findViewById<Button>(R.id.btn_carrillo)
         val bCancun = findViewById<Button>(R.id.btn_cancun)
 
+        val bShanghai = findViewById<Button>(R.id.btn_shanghai)
+
+
         bPlaya.setOnClickListener(View.OnClickListener {
             //hacia donde queremos ir
             val intent = Intent(this, MainActivity::class.java)
@@ -45,5 +48,13 @@ class Ciudades : AppCompatActivity() {
             intent.putExtra(TAG, "3531673")
             startActivity(intent)
         })
+        //1796236
+
+        bShanghai.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "1796236")
+            startActivity(intent)
+        })
+
     }
 }
